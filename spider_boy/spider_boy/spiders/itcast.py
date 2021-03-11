c2224055e0982ce01ac60523d6e244e7144b77ed
150619 <rtyhxrtyhrtyh78782@gmail.com>
2021-03-11 22:07:46 +0800

    robots协议改为False   设置USER_AGENT

--- spider_boy/spider_boy/spiders/itcast.py
+++ spider_boy/spider_boy/spiders/itcast.py
@@ -1,6 +1,10 @@
 import scrapy
 
 
+# name = //div[@class="main_mask"]/h2
+# time = //div[@class="main_mask"]/h3
+# describe = //div[@class="main_mask"]/p
+
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
